2ae9b461843f1bcf7103d1f5dd707a1c2f8e11b8
OkadaShotaro <b1016117@fun.ac.jp>
2019-03-29 19:41:52 +0900

    チュートリアル7章まで完了

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: '名前ジェネレーター',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
